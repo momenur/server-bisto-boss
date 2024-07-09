afdec0c0c62d365664a865c7528f2a0e68597006
momenur <momenurislam6@gmail.com>
2024-07-09 11:40:29 +0600

    dkfjkdjfklsdj

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// require("dotenv").config();
+// const port = process.env.PORT || 5000;
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 const port = process.env.PORT || 5000;
 
 // middle Ware
+// app.use(cors());
+// app.use(express.json());
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vrpqnh1.mongodb.net/?retryWrites=true&w=majority`;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vrpqnh1.mongodb.net/?retryWrites=true&w=majority`;
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
